e256f1e048892438e7199059da4a68e62bf77cd0
macedaash <macedaashlley@gmail.com>
2026-05-25 20:22:40 +0800

    Add deployment files

--- config/settings.py
+++ config/settings.py
@@ -96,3 +96,10 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 LOGIN_URL = 'login'
 LOGIN_REDIRECT_URL = 'album-list'
 LOGOUT_REDIRECT_URL = 'login'
+
+
+if not DEBUG:
+    SECURE_PROXY_SSL_HEADER = ('HTTP_X_FORWARDED_PROTO', 'https')
+    SECURE_SSL_REDIRECT = True
+    SESSION_COOKIE_SECURE = True
+    CSRF_COOKIE_SECURE = True
